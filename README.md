fa8c9c20cc559f02d002434e81ed06bad3186f1d
Brian Muckian <bmuckian@gmail.com>
2022-04-06 07:47:32 -0700

    README has a few working curl examples, factored out flask-restful, added prometheus metrics; GET/PUT/POST methods on data endpoint perform rudimentary operations: list/retrieve row(s); append to a list of a given row; add a row. needs error handling, testing, etc.

--- README.md
+++ README.md
@@ -14,10 +14,27 @@ sudo apt install python3-venv python3-virtualenv
 python3 -m venv ./venv
 source ./venv/bin/activate
 
-pip install flask-restful
+pip install prometheus-flask-exporter
 
-python3 ./flask-restful-api.py
+python3 ./flask-data-api.py
 
 ```
 
 the last command above will serve flask app on http://0.0.0.0:5000/data
+
+
+some sample curl calls (my hostname is `debian20220207aa`):
+```
+# get all rows
+curl http://debian20220207aa:5000/data
+
+# get one row
+curl http://debian20220207aa:5000/data/d1
+
+# post data to create a new row
+curl -X POST -H "Content-type: application/json" -d "{\"data\" : [1, 23, \"purple\", 65] }" http://debian20220207aa:5000/data
+
+# put new data on an existing row
+curl -X PUT -H "Content-type: application/json" -d "{\"data\" : [1, 23, \"green\", 65] }" http://debian20220207aa:5000/data/d1
+
+```
--- flask-data-api.py
+++ flask-data-api.py
@@ -5,7 +5,6 @@ import random
 from flask import Flask, request
 from prometheus_flask_exporter import PrometheusMetrics
 
-
 app = Flask(__name__)
 PrometheusMetrics(app)
 
@@ -14,19 +13,39 @@ DATA = {
     "d2": [2, 33, 4]
     }
 
-@app.route("/")
+
+@app.route("/data")
 def list_data():
     return json.dumps(DATA)
 
 
-@app.route('/data', methods=['POST', 'PUT'])
-def process_json():
+@app.route('/data/<data_id>', methods=['GET', 'PUT'])
+def get_data(data_id):
+    content_type = request.headers.get('Content-Type')
+    if (request.method == 'PUT'):
+        if (content_type == 'application/json'):
+            print("append new data to row: {}".format(data_id))
+            d = DATA[data_id]
+            j = request.json
+            DATA[data_id] = [ *d, *j["data"] ]
+            return json.dumps(DATA[data_id])
+        else:
+            return 'Content-Type not supported!'
+    else:
+        print("get the data row: {}".format(data_id))
+        return json.dumps(DATA[data_id])
+
+
+@app.route('/data', methods=['POST'])
+def process_data():
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
-        json = request.json
-        return json
+        print("add a new row with id: d{}".format(len(DATA)))
+        j = request.json
+        return j
     else:
         return 'Content-Type not supported!'
+
 
 if __name__ == "__main__":
     app.run("0.0.0.0", 5000, threaded=True)
